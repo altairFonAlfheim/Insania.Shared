ca6546ca9f41047e2e6b3e45b5f1c5d873d8eef3
insaniaOfficialis <insania_officialis@vk.com>
2025-04-10 10:52:38 +0300

    - Добавлен интерфейс инициализации данных - Обновлены пакеты

---
+++ Insania.Shared.Contracts/DataAccess/IInitializationDAO.cs
@@ -0,0 +1,12 @@
+﻿namespace Insania.Shared.Contracts.DataAccess;
+
+/// <summary>
+/// Интерфейс инициализации данных
+/// </summary>
+public interface IInitializationDAO
+{
+    /// <summary>
+    /// Метод инициализации данных
+    /// </summary>
+    Task Initialize();
+}
