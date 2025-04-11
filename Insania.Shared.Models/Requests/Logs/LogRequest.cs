310009bca3a274c70bbd4b80c1bc09f492d11fd7
insaniaOfficialis <insania_officialis@vk.com>
2025-04-11 15:14:17 +0300

    - Добавлена модель лога запросов

---
+++ Insania.Shared.Models/Requests/Logs/LogRequest.cs
@@ -0,0 +1,25 @@
+﻿namespace Insania.Shared.Models.Requests.Logs;
+
+/// <summary>
+/// Модель запроса для записи в лог
+/// </summary>
+/// <param cref="string?" name="token">Токен запроса</param>
+/// <param cref="string?" name="queryParams">Параметры строки</param>
+/// <param cref="string?" name="body">Тело запроса</param>
+public class LogRequest (string? token = null, string? queryParams = null, string? body = null)
+{
+    /// <summary>
+    /// Токен запроса
+    /// </summary>
+    public string? Token { get; set; } = token;
+
+    /// <summary>
+    /// Параметры строки
+    /// </summary>
+    public string? QueryParams { get; set; } = queryParams;
+
+    /// <summary>
+    /// Тело запроса
+    /// </summary>
+    public string? Body { get; set; } = body;
+}
