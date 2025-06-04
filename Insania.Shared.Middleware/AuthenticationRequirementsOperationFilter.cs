e8a810f77c37d2a1e1feb995368ab8d33f92bacb
altairFonAlfheim <insania_officialis@outlook.com>
2025-06-04 14:50:24 +0300

    - Обновлены пакеты - Перенесено изображение пакетов в решение - Добавлен пакет для общих конвееров запросов

---
+++ Insania.Shared.Middleware/AuthenticationRequirementsOperationFilter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.OpenApi.Models;
+
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Insania.Shared.Middleware;
+
+/// <summary>
+/// Класс фильтров операции для сваггера
+/// </summary>
+public class AuthenticationRequirementsOperationFilter : IOperationFilter
+{
+    /// <summary>
+    /// Метод применение токена
+    /// </summary>
+    /// <param cref="OpenApiOperation" name="operation">Операция</param>
+    /// <param cref="OperationFilterContext" name="context">Контекст запроса</param>
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        operation.Security ??= [];
+
+        OpenApiSecurityScheme scheme = new()
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        };
+
+        operation.Security.Add(new OpenApiSecurityRequirement { [scheme] = [] });
+    }
+}
