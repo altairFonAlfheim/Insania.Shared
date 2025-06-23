2fff6e5af110c26050797e94f72ecca22dabcdee
altairFonAlfheim <insania_officialis@outlook.com>
2025-06-23 21:36:35 +0300

    -Добавлены поля координат центра сущности и её масштаба

--- Insania.Shared.Entities/EntityCoordinate.cs
+++ Insania.Shared.Entities/EntityCoordinate.cs
@@ -2,6 +2,8 @@
 
 using Microsoft.EntityFrameworkCore;
 
+using NetTopologySuite.Geometries;
+
 namespace Insania.Shared.Entities;
 
 /// <summary>
@@ -15,6 +17,7 @@ public abstract class EntityCoordinate : Reestr
     /// </summary>
     public EntityCoordinate() : base()
     {
+        Center = new(new());
     }
 
     /// <summary>
@@ -22,10 +25,14 @@ public abstract class EntityCoordinate : Reestr
     /// </summary>
     /// <param cref="string" name="username">Логин пользователя, выполняющего действие</param>
     /// <param cref="bool" name="isSystem">Признак системной записи</param>
+    /// <param cref="Point" name="center">Координаты точки центра сущности</param>
+    /// <param cref="int" name="zoom">Коэффициент масштаба отображения сущности</param>
     /// <param cref="Coordinate" name="coordinate">Координата</param>
     /// <param cref="DateTime?" name="dateDeleted">Дата удаления</param>
-    public EntityCoordinate(string username, bool isSystem, Coordinate coordinate, DateTime? dateDeleted = null) : base(username, isSystem, dateDeleted)
+    public EntityCoordinate(string username, bool isSystem, Coordinate coordinate, Point center, int zoom, DateTime? dateDeleted = null) : base(username, isSystem, dateDeleted)
     {
+        Center = center;
+        Zoom = zoom;
         CoordinateId = coordinate.Id;
         CoordinateEntity = coordinate;
     }
@@ -36,16 +43,34 @@ public abstract class EntityCoordinate : Reestr
     /// <param cref="long" name="id">Первичный ключ таблицы</param>
     /// <param cref="string" name="username">Логин пользователя, выполняющего действие</param>
     /// <param cref="bool" name="isSystem">Признак системной записи</param>
+    /// <param cref="Point" name="center">Координаты точки центра сущности</param>
+    /// <param cref="int" name="zoom">Коэффициент масштаба отображения сущности</param>
     /// <param cref="Coordinate" name="coordinate">Координата</param>
     /// <param cref="DateTime?" name="dateDeleted">Дата удаления</param>
-    public EntityCoordinate(long id, string username, bool isSystem, Coordinate coordinate, DateTime? dateDeleted = null) : base(id, username, isSystem, dateDeleted)
+    public EntityCoordinate(long id, string username, bool isSystem, Coordinate coordinate, Point center, int zoom, DateTime? dateDeleted = null) : base(id, username, isSystem, dateDeleted)
     {
+        Center = center;
+        Zoom = zoom;
         CoordinateId = coordinate.Id;
         CoordinateEntity = coordinate;
     }
     #endregion
 
     #region Поля
+    /// <summary>
+    /// Координаты точки центра сущности
+    /// </summary>
+    [Column("center")]
+    [Comment("Координаты точки центра сущности")]
+    public Point Center { get; private set; }
+
+    /// <summary>
+    /// Коэффициент масштаба отображения сущности
+    /// </summary>
+    [Column("zoom")]
+    [Comment("Коэффициент масштаба отображения сущности")]
+    public int Zoom { get; private set; }
+
     /// <summary>
     /// Идентификатор координаты
     /// </summary>
@@ -63,6 +88,18 @@ public abstract class EntityCoordinate : Reestr
     #endregion
 
     #region Методы
+    /// <summary>
+    /// Метод записи координат точки центра сущности
+    /// </summary>
+    /// <param cref="Point" name="center">Координаты точки центра сущности</param>
+    public void SetCenter(Point center) => Center = center;
+
+    /// <summary>
+    /// Метод записи коэффициент масштаба отображения
+    /// </summary>
+    /// <param cref="int" name="zoom">Коэффициент масштаба отображения сущности</param>
+    public void SetZoom(int zoom) => Zoom = zoom;
+
     /// <summary>
     /// Метод записи координаты
     /// </summary>
