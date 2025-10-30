7d5a5bc7d1fe915af4fe323eb7f2c946fb849a1a
Альтаир фон Альфхейм <insania_officialis@outlook.com>
2025-10-30 09:01:21 +0300

    - Обновление пакетов - Добавление ссылки на файл в базовую модель элемента ответа списком

--- Insania.Shared.Models/Responses/Base/BaseResponseListItem.cs
+++ Insania.Shared.Models/Responses/Base/BaseResponseListItem.cs
@@ -6,7 +6,8 @@
 /// <param cref="long" name="id">Идентификатор сущности</param>
 /// <param cref="string" name="name">Наименование сущности</param>
 /// <param cref="string?" name="description">Описание сущности</param>
-public class BaseResponseListItem(long id, string name, string? description = null)
+/// <param cref="string?" name="file">Ссылка на файл сущности</param>
+public class BaseResponseListItem(long id, string name, string? description = null, string? file = null)
 {
     /// <summary>
     /// Идентификатор сущности
@@ -22,4 +23,9 @@ public class BaseResponseListItem(long id, string name, string? description = nu
     /// Описание сущности
     /// </summary>
     public string? Description { get; set; } = description;
+
+    /// <summary>
+    /// Ссылка на файл сущности
+    /// </summary>
+    public string? File { get; set; } = file;
 }
